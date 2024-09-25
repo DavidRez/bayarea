b49f79cd328c11f94ce94385f59b3ffa0f8e677a
David Ramirez <davidraviles@gmail.com>
2024-09-25 15:11:45 -0700

    updates(block-column-tabs-mobile): modified build.

--- components/block/block-column-tabs/block-column-tabs-mobile/index.vue
+++ components/block/block-column-tabs/block-column-tabs-mobile/index.vue
@@ -13,14 +13,9 @@ export default {
   },
   data: () => ({
     accordionBodyHeight: null,
-    expanded: null
+    expanded: 0
   }),
   mounted () {
-    this.$nextTick(() => {
-      this.toggleAccordion(0)
-      this.handleResize(1000)
-    })
-
     if (this.$store.state.siteIsLoaded) {
       this.handleAnimation()
     } else {
@@ -34,23 +29,9 @@ export default {
       )
     }
     this.handleAnimation()
-
-    window.addEventListener('resize', () => {
-      this.handleResize()
-    })
-  },
-  destroyed () {
-    window.removeEventListener('resize', this.handleResize)
   },
   methods: {
     handleAnimation () {
-      const tl = this.$gsap.timeline({
-        scrollTrigger: {
-          trigger: this.$refs.container,
-          start: 'top+=48 bottom',
-          toggleActions: 'play none play none'
-        }
-      })
       if (this.$refs.gsap1) {
         this.$_fadeIn(this.$refs.gsap1, 0, 24, 'top+=58', 0, 1.2)
       }
@@ -59,22 +40,12 @@ export default {
           this.$_fadeIn(el, 0, 24, 'top+=58', 0, 1.2)
         })
       }
-      tl.add(() => {
-        this.toggleAccordion(0)
-      })
-    },
-    handleResize (time = 300) {
-      setTimeout(() => {
-        this.accordionBodyHeight = this.$refs.content[this.expanded].clientHeight
-      }, time)
     },
     toggleAccordion (i) {
       this.$nextTick(() => {
         if (this.expanded !== i) {
           this.expanded = i
-          this.handleResize(0)
         }
-        this.handleResize(500)
       })
     }
   }
