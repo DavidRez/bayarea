6bb52d767a8cfcb24988e27f0f074012783f4962
David Ramirez <davidraviles@gmail.com>
2024-09-26 12:54:49 -0700

    updates(404): updated button

--- pages/404/index.vue
+++ pages/404/index.vue
@@ -1,20 +1,20 @@
 <template lang='pug'>
-  .page-404(:class="{ 'page-404--dark': darkMode }")
-    .page-404__container(ref='container')
-      // Classic Theme
-      .page-404__column(v-if='theme === "classic"')
-        h1.page-404__title 404
-        h2.page-404__subtitle Page Not Found
-        p.page-404__text The link you followed my be broken or the page may have been removed.
-        nuxt-link.page-404__button(to='/', aria-label='Back to home.') Back To Home
+.page-404(:class="{ 'page-404--dark': darkMode }")
+  .page-404__container(ref='container')
+    // Classic Theme
+    .page-404__column(v-if='theme === "classic"')
+      h1.page-404__title 404
+      h2.page-404__subtitle Page Not Found
+      p.page-404__text The link you followed my be broken or the page may have been removed.
+      ButtonButtonCta.page-404__button(:props='Object.assign({}, { style: "primary", label: "Back to Home", path: "/", type: "nuxt" })')
 
-      //Branded Theme
-      .page-404__content(v-else-if='theme === "branded"')
-        nuxt-link.page-404__logo(to='/', aria-label='Return Home')
-          BaseIcon.page-404__logo-icon(name='logo' :class="{ 'page-404__logo-icon--portrait': logoPortrait }")
-        h1.page-404__title.page-404__title--branded Whoops!
-        p.page-404__text.page-404__text--branded We can't seem to find the page you are looking for
-        nuxt-link.page-404__button(to='/', aria-label='Return Home') Return Home
+    //Branded Theme
+    .page-404__content(v-else-if='theme === "branded"')
+      nuxt-link.page-404__logo(to='/', aria-label='Return Home')
+        BaseIcon.page-404__logo-icon(name='logo' :class="{ 'page-404__logo-icon--portrait': logoPortrait }")
+      h1.page-404__title.page-404__title--branded Whoops!
+      p.page-404__text.page-404__text--branded We can't seem to find the page you are looking for
+      ButtonButtonCta.page-404__button(:props='Object.assign({}, { style: "primary", label: "Back to Home", path: "/", type: "nuxt" })')
 </template>
 
 <script>
